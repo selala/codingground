03cf1e90474a3e6847efe6ced6c8441b5fd15d85
selala <selanurismia@gmail.com>
2017-02-27 12:34:45 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas5.java
@@ -0,0 +1,17 @@
+public class Tugas5{
+
+     public static void main (String []args){
+         int a = 7;
+         int hasil = HitungMundur(a);
+         for (int x= hasil; x>=1; x--){
+              System.out.println(x);
+         }
+
+     }
+     public static int HitungMundur (int a1){
+         int angka = a1;
+         return angka;
+        
+     }
+     
+}
