019955697707c6618718358e240d0761cb9b6616
selala <selanurismia@gmail.com>
2017-02-27 12:35:31 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas2.java
@@ -0,0 +1,10 @@
+public class Tugas2{
+
+     public static void main(String []args){
+         String[] nama ={"Chopin", "Upin", "Ipin"};
+         
+         System.out.println ("Peserta 1 = " + nama[0]);
+         System.out.println ("Peserta 2 = " + nama[1]);
+         System.out.println ("Peserta 3 = " + nama[2]);
+     }
+}
