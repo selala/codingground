266b1393c1683d9921c1c31f5bae49144b1900e9
selala <selanurismia@gmail.com>
2017-03-04 08:56:15 +0700

    Update Tugas6.java

--- pretest pemahaman java/Tugas6.java
+++ pretest pemahaman java/Tugas6.java
@@ -1,6 +1,20 @@
 public class Tugas6{
+	public static void main(String []args){
+	Scanner scanner = new Scanner(System.in);
+	System.out.print("Masukkan Nilai anda = ");
+	int n = scanner.nextInt();
+	String hasil = Tampilkan(n);
+	System.out.println("Nilai yang anda masukkan adalah = " + n + "berarti anda " + hasil);
 
-    public static void main (String []args){
-     Scanner scanner = new Scanner (System)  
-    }
+}
+	public static String Tampilkan(int a){
+	int n = a;
+
+	String hasil;
+	if (n<60){
+	hasil = "tidak lulus";} else{ hasil = "Lulus";}
+
+	return hasil;
+}
+
 }
